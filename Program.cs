d470ef87866b71d53fe0a39278dab5bc0dcf66bc
suthardevanshi608 <suthardevanshi608@gmail.com>
2025-12-17 16:03:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Value_Vs_Reference
+{
+    internal class Student
+    {
+        public int Id;
+        public string Name;
+    }
+    class Program
+    {
+        private static int x;
+
+        static void main()
+        {
+            int x = 10;
+            int y = x;
+            y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+        }  
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student();
+            s1.Name = "John";
+            Student s2 = s1;
+            s2.Name = "Doe";
+            Console.WriteLine(s1.Name);
+            Console.WriteLine(s2.Name);
+        }
+    }
+}
