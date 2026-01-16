87024d0e816ec4dd940b02e7ae928fd3c33d1bd8
suthardevanshi608 <devanshi@evisionits.com>
2026-01-16 16:19:26 +0530

    Add files via upload

---
+++ ToDo.cs
@@ -0,0 +1,13 @@
+﻿using Sieve.Attributes;
+namespace TechDemo
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Sieve(CanFilter = true, CanSort = true)]]
+        public string Name { get; set; }
+        public bool Description { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using Sieve.Attributes;
+
+namespace TechDemo.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Sieve(CanFilter = true, CanSort = true)]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
